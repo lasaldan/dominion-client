5bcc7e968474ea5cb774b6ce18ab5a58f3268893
Daniel Fuller <lasaldan@gmail.com>
2019-08-29 22:46:13 -0600

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
-// var SERVER_URL = "https://private-dominion-server.herokuapp.com"
-var SERVER_URL = "http://localhost:8081"
+var SERVER_URL = "https://private-dominion-server.herokuapp.com"
+// var SERVER_URL = "http://localhost:8081"
 
 var server = new Server()
 var gameUI = new GameDOM()
