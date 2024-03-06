c743335244bf7a2459ec15688a78e9ec5144b7de
Daniel Fuller <daniel.fuller@tyfoom.com>
2024-03-06 14:07:13 -0700

    Update to render URL

--- client.js
+++ client.js
@@ -1,5 +1,8 @@
-var SERVER_URL = "https://private-dominion-server.herokuapp.com"
+// var SERVER_URL = "https://private-dominion-server.herokuapp.com"
+
 // var SERVER_URL = "http://localhost:8081"
+
+var SERVER_URL = "https://dominion-server.onrender.com"
 
 var server = new Server()
 var gameUI = new GameDOM()
